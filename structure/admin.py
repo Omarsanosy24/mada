0ad9f01b7184327789d53f9b1ff992f4c915b530
Omarsanosy24 <113290018+Omarsanosy24@users.noreply.github.com>
2025-05-03 19:30:16 +0300

    Register ServicesModel in the admin interface deploy

--- structure/admin.py
+++ structure/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import StaticData
+from .models import StaticData, ServicesModel
 # Register your models here.
 
 
 @admin.register(StaticData)
 class StaticDataAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ServicesModel)
+class ServicesModelAdmin(admin.ModelAdmin):
     pass
