436c046283efa4fa3af4879c5d4768be2d0d5595
Omarsanosy24 <113290018+Omarsanosy24@users.noreply.github.com>
2025-05-09 00:59:43 +0300

    Add H_Vac_CategoryViewSet and OurClientsView to URL routing deploy

--- structure/urls.py
+++ structure/urls.py
@@ -1,12 +1,15 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import StaticDataViewSet, BannersView, BlogsView, ProductView
+from .views import StaticDataViewSet, BannersView, BlogsView, ProductView, H_Vac_CategoryViewSet, OurClientsView
 
 router = DefaultRouter()
 router.register("static-data", StaticDataViewSet)
 router.register("banners", BannersView)
 router.register("blogs", BlogsView)
 router.register("product", ProductView)
+router.register("h-vac", H_Vac_CategoryViewSet)
+router.register("our-clients", OurClientsView)
+
 
 app_name = "static_data"
 urlpatterns = [
